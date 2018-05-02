8ffde268da46343b7a7c86748c1f7133f1fdf5f7
jinxinchen <jinxinchen@ymail.com>
2018-05-02 15:07:30 -0400

    add IndentityModels
    <connectonString>
    <add name="DefaultConnection" connectionString="Data Source=(local)\sqle2012;Initial Catalog=UserDB;Integrated Security=True" providerName="s">
    </add>
    </connectionString>

---
+++ aspwebapp1/aspwebapp1/Models/IdentityModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace aspwebapp1.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        /*
+         *"DefaultConnection" is a string in Web.config
+        <connectonString>
+		<add name="DefaultConnection" connectionString="Data Source=(local)\sqle2012;Initial Catalog=UserDB;Integrated Security=True" providerName="s">
+			
+		</add>
+	</connectonString>
+         */
+        public ApplicationDbContext() : 
+            base("DefaultConnection", throwIfV1Schema: false) {
+        }
+    }
+}
